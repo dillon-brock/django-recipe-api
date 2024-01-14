085dd1ee920b2039b6137d91fe0ea0dd013a07f9
Dillon Brock <mdillon1297@gmail.com>
2024-01-14 15:55:16 -0800

    Implement manage user API
    Add a /me endpoint to our user API endpoints, which allows users to get and update their data. All corresponding unit tests are passing.

--- app/user/urls.py
+++ app/user/urls.py
@@ -3,12 +3,13 @@ URL mappings for the user API.
 """
 from django.urls import path
 
-from user.views import (CreateUserView, CreateTokenView)
+from user import views
 
 
 app_name = 'user'
 
 urlpatterns = [
-  path('create/', CreateUserView.as_view(), name='create'),
-  path('token/', CreateTokenView.as_view(), name='token')
+  path('create/', views.CreateUserView.as_view(), name='create'),
+  path('token/', views.CreateTokenView.as_view(), name='token'),
+  path('me/', views.ManageUserView.as_view(), name='me')
 ]
