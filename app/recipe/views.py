a34da72528604c431be8b8f45caa8ecbd757cb1f
Dillon Brock <mdillon1297@gmail.com>
2024-01-21 09:58:54 -0800

    Add functionality for updating ingredient

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -35,10 +35,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
     return serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.UpdateModelMixin,
-                 mixins.ListModelMixin,
-                 mixins.DestroyModelMixin,
-                 viewsets.GenericViewSet):
+class TagViewSet(
+  mixins.UpdateModelMixin,
+  mixins.ListModelMixin,
+  mixins.DestroyModelMixin,
+  viewsets.GenericViewSet
+):
   """Manage tags in the database."""
   serializer_class = serializers.TagSerializer
   queryset = Tag.objects.all()
@@ -50,7 +52,11 @@ class TagViewSet(mixins.UpdateModelMixin,
     return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class IngredientViewSet(
+  mixins.ListModelMixin,
+  mixins.UpdateModelMixin,
+  viewsets.GenericViewSet
+):
   """Manage ingredients in the database."""
   serializer_class = serializers.IngredientSerializer
   queryset = Ingredient.objects.all()
